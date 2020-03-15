1939bdbaf351a29d879c687ab6f8e9816589cf64
lanhsotret <58644190+lanhsotret@users.noreply.github.com>
2020-03-15 17:02:17 +0700

    again modifer

--- JS/appendHtml.js
+++ JS/appendHtml.js
@@ -103,7 +103,7 @@ const translate_js = (function() {
 class appendHTML {
   constructor(tagHTML, eleTarget, content, nameclass) {
     this._tagHTML = document.createElement(tagHTML);
-    this._eleTarget = document.querySelectorAll(eleTarget);
+    this._eleTarget = document.querySelector(eleTarget);
     this._content = content;
     this._nameClass = nameclass;
   }
@@ -116,9 +116,7 @@ class appendHTML {
   apply() {
     this._tagHTML.classList.add(this._nameClass);
     this._tagHTML.innerHTML = this._content;
-    this._eleTarget.forEach(particle => {
-      particle.appendChild(this._tagHTML);
-    });
+    this._eleTarget.appendChild(this._tagHTML);
   }
 }
 
@@ -150,40 +148,192 @@ genre.modifier = `<div class="div-genre">action</div>
 
 genre.apply();
 
-appendHTML.prototype.eventClick = function(callback1, callback2) {
-  this._eleTarget.forEach(particle => {
-    particle.addEventListener("click", () => {
-      callback1(this._tagHTML, particle);
-    });
-  });
-  this._eleTarget.forEach(particle => {
-    particle.addEventListener("mouseleave", () => {
-      callback2(this._tagHTML, particle);
-    });
-  });
+appendHTML.prototype.event = function(callback1, callback2, eleTarget) {
+  callback1(this._tagHTML, eleTarget);
+  callback2(this._tagHTML, eleTarget);
 };
 
-const modifierCSS = (function() {
-  function eventClick(ele, eleRelate) {
-    if (ele.style.display == "block") {
-      ele.style.display = "none";
-      ele.style.opacity = 0;
-      eleRelate.classList.remove("active");
-    } else {
-      ele.style.display = "block";
-      ele.style.opacity = 1;
-      eleRelate.classList.add("active");
-    }
+const eventCallback = (function() {
+  function click(ele, eleTarget) {
+    let domTarget = document.querySelector(eleTarget);
+    domTarget.addEventListener("click",()=>{
+      if (ele.style.visibility == "visible") {
+        ele.style.visibility = "hidden";
+        ele.style.opacity = 0;
+        domTarget.classList.remove("active");
+      } else {
+        ele.style.visibility = "visible";
+        ele.style.opacity = 1;
+        domTarget.classList.add("active");
+      }
+    });
   }
-  function eventmouseout(ele, eleRelate) {
-    ele.style.display = "none";
-    ele.style.opacity = 0;
-    eleRelate.classList.remove("active");
+  function windowClick(ele, eleTarget){
+    let domTarget = document.querySelector(eleTarget);
+    window.addEventListener("click", (e)=>{
+      if(!ele.contains(e.target) && !domTarget.contains(e.target)){
+        ele.style.visibility = "hidden";
+        ele.style.opacity = 0;
+        domTarget.classList.remove("active");
+      }
+    });
   }
   return {
-    clickStyle: eventClick,
-    mouseoutStyle: eventmouseout
+    click: click,
+    clickOut: windowClick
   };
 })();
 
-genre.eventClick(modifierCSS.clickStyle, modifierCSS.mouseoutStyle);
+genre.event(eventCallback.click, eventCallback.clickOut, ".nav__div.nav__genre");
+
+let release = new appendHTML("div", "nav.nav", "content", "nav__release_js");
+release.modifier = function(){
+  let content = "";
+  for(let i = 2000; i <= 2020; i++) {
+    content += `<div>${i}</div>`;
+  }
+  return `<section class="nav__year-section">
+      <span>year</span>
+      <i class="fa fa-caret-left" aria-hidden="true"></i>
+      <span>2000+</span>
+      <i class="fa fa-caret-right" aria-hidden="true"></i>
+    </section>
+    <div class="nav__year-div">${content}</div>
+    <div class="nav__year-circle">
+      <div></div><div></div><div></div><div></div><div></div>
+    </div>`
+}();
+
+release.apply();
+release.event(eventCallback.click, eventCallback.clickOut, ".nav__div.nav__release");
+
+let gallery = new appendHTML("div", "nav.nav", "content", "nav__setGallery");
+gallery.modifier = `<div class="nav__gallery-div">
+    <section class="nav__gallery-section gallery-romance">
+      <div class="nav__img-div">
+        <figure class="nav__gallery-figure"></figure>
+        <figure class="nav__gallery-figure"></figure>
+        <figure class="nav__gallery-figure"></figure>
+      </div>
+      <div class="nav__gallery-line"></div>
+      <div class="nav__gallery-title">romances</div>
+    </section>
+    <section class="nav__gallery-section gallery-war">
+      <div class="nav__img-div">
+        <figure class="nav__gallery-figure"></figure>
+        <figure class="nav__gallery-figure"></figure>
+        <figure class="nav__gallery-figure"></figure>
+      </div>
+      <div class="nav__gallery-line"></div>
+      <div class="nav__gallery-title">wars</div>
+    </section>
+    <section class="nav__gallery-section gallery-fantasy">
+      <div class="nav__img-div">
+        <figure class="nav__gallery-figure"></figure>
+        <figure class="nav__gallery-figure"></figure>
+        <figure class="nav__gallery-figure"></figure>
+      </div>
+      <div class="nav__gallery-line"></div>
+      <div class="nav__gallery-title">fantasy</div>
+    </section>
+  </div>
+  <div class="nav__gallery-circle">
+      <div></div><div></div><div></div><div></div><div></div>
+  </div>
+  <a class="nav__gallery-a next-a"><span class="nav__gallery-span span-next"></span></a>
+  <a class="nav__gallery-a previous-a"><span class="nav__gallery-span span-previous"></span></a>`
+
+gallery.apply();
+gallery.event(eventCallback.click, eventCallback.clickOut, ".nav__div.nav__gallery");
+
+let news = new appendHTML("div", "nav.nav", "content", "nav__news-primary");
+news.modifier = function(){
+  let content = "";
+  for(let key in newsData) {
+    let tagContent = "";
+    for(let index in newsData[key].img) {
+      console.log(newsData[key].img[index]);
+      tagContent += `<div class="nav__news-Abox ${key}-news${+index + 1}">
+        <figure class="nav__news-figure" style="background-image: url('${newsData[key].img[index]}')"></figure>
+        <div class="nav__news-boxTitle">
+          <p class="nav__news-title">${newsData[key].title[index]}</p>
+          <div class="nav__news-line"></div>
+          <p class="nav__news-content">${newsData[key].content[index].substring(0, 100) + "..."}</p>
+        </div>
+      </div>`
+    }
+    content += `<section class="nav__news-section section-${key}">
+      <div class="nav__news-name">${key}</div>
+      <div class="nav__news-board newsboard-${key}">${tagContent}</div>
+      <div class="nav__news-arrow arrowDown-div">
+        <span class="nav__news-span arrowDown-span"></span>
+      </div>
+    </section>`
+  }
+  return `<div class="nav__news-child">${content}</div>
+    <div class="nav__news-circle">
+      <div></div><div></div><div></div><div></div><div></div>
+    </div>`;
+}();
+news.apply();
+
+const moveStyle = (function(){
+  function translate(eleTarget, btn1) {
+    let domTarget = document.querySelectorAll(eleTarget);
+    let btn1Target = document.querySelector(btn1);
+    let css = getComputedStyle(domTarget[0]);
+    let widthMove = domTarget[0].clientHeight + parseInt(css.marginTop) * 2;
+    let count = 0;
+    btn1Target.addEventListener("click", ()=>{
+      count--;
+      count < 0? count = 2: "";
+      domTarget[count].style.transform = `translateY(-${widthMove * (count + 1)}px)`;
+      domTarget[count + 1 > 2? 0: count + 1].style.zIndex = 3;
+      domTarget.forEach((particle,index)=>{
+        index == (count + 1 > 2? 0: count + 1)? "": particle.style.zIndex = 0;
+        let transStyle = particle.style.transform? parseInt(particle.style.transform.split("(")[1]): 0;
+        particle.style.transform = `translateY(${transStyle + widthMove}px)`;
+      });
+    });
+  };
+  function translate2(nameEle, btn) {
+    let eleTarget = document.querySelectorAll(nameEle);
+    let btnTarget = document.querySelector(btn);
+    let css = getComputedStyle(eleTarget[0]);
+    let transMove = eleTarget[0].offsetHeight + parseInt(css.marginTop) * 2;
+    console.log(transMove);
+    eleTarget.forEach((particle,index)=>{
+      particle.style.zIndex = 2 - index;
+      if(index !== 0)
+        particle.style.transform = `translateY(-${(index - 1) * transMove}px)`;
+    });
+    let count = 0;
+    btnTarget.addEventListener("click", ()=>{
+      let transStyle = eleTarget[count].style.transform? parseInt(eleTarget[count].style.transform.split("(")[1]): 0;
+      eleTarget[count].style.transform = `translateY(${transStyle + transMove}px)`;
+      eleTarget[count + 1 > 2? 0: count +1].style.transform = `translateY(${parseInt(eleTarget[count + 1 > 2? 0: count +1].style.transform.split("(")[1]) - transMove}px)`
+      count++;
+      count > 2? count = 0: "";
+      eleTarget[count].addEventListener("transitionend", function myevent(e){
+        e.currentTarget.style.zIndex = 2;
+        eleTarget.forEach((particle,index)=>{
+          if(index !== count){
+            particle.style.zIndex = 0;
+          }
+        });
+        e.currentTarget.removeEventListener("transitionend", myevent);
+      });
+    });
+    
+  }
+  return {translate: translate,
+    translate2: translate2
+    };
+})();
+
+news.event(eventCallback.click, eventCallback.clickOut, ".nav__div.nav__news");
+moveStyle.translate2(".section-anime .nav__news-Abox", ".section-anime .arrowDown-div");
+moveStyle.translate2(".section-movie .nav__news-Abox", ".section-movie .arrowDown-div");
+moveStyle.translate2(".section-Tvseries .nav__news-Abox", ".section-Tvseries .arrowDown-div");
+
+
