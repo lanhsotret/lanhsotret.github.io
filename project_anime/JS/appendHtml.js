c4946e65636535a5f3f1a686fcfad630510bb0d6
lanhsotret <58644190+lanhsotret@users.noreply.github.com>
2020-03-17 17:11:19 +0700

    modifer

--- project_anime/JS/appendHtml.js
+++ project_anime/JS/appendHtml.js
@@ -101,11 +101,14 @@ const translate_js = (function() {
 // translate_js.animation('.sideshow-popular__div','.sideshow-popular__button.div-button__bt1', '.sideshow-popular__button.div-button__bt2', 2);
 
 class appendHTML {
-  constructor(tagHTML, eleTarget, content, nameclass) {
+  constructor(tagHTML, eleTarget, nameclass, content) {
     this._tagHTML = document.createElement(tagHTML);
-    this._eleTarget = document.querySelector(eleTarget);
-    this._content = content;
+    this._nameTag = tagHTML;
+    this._eleTarget = document.querySelectorAll(eleTarget);
     this._nameClass = nameclass;
+    this._content = function() {
+      content(arguments[0], arguments[1], arguments[2]);
+    };
   }
   get tagHTML() {
     return this._tagHTML;
@@ -114,140 +117,401 @@ class appendHTML {
     this._content = newContent;
   }
   apply() {
-    this._tagHTML.classList.add(this._nameClass);
-    this._tagHTML.innerHTML = this._content;
-    this._eleTarget.appendChild(this._tagHTML);
+    this._eleTarget.forEach((particle, index) => {
+      let eleCreate = document.createElement(this._nameTag);
+      eleCreate.className += this._nameClass;
+      this._content(eleCreate, particle, index);
+    });
   }
 }
+const ApplyContent = (function() {
+  function genre(ele1, ele2) {
+    ele1.innerHTML = `<div class="div-genre">action</div>
+      <div class="div-line"></div>
+      <div class="div-genre">adventure</div>
+      <div class="div-line"></div>
+      <div class="div-genre">tragedy</div>
+      <div class="div-line"></div>
+      <div class="div-genre">history</div>
+      <div class="div-line"></div>
+      <div class="div-genre">horror</div>
+      <div class="div-line"></div>
+      <div class="div-genre">mystery</div>
+      <div class="div-line"></div>
+      <div class="div-genre">spsychology</div>
+      <div class="div-line"></div>
+      <div class="div-genre">cartoon</div>
+      <div class="div-circle">
+      <div></div><div></div><div></div><div></div><div></div>
+      </div>`;
+    ele2.appendChild(ele1);
+  }
+  function release(ele1, ele2) {
+    let content = "";
+    for (let i = 2000; i <= 2020; i++) {
+      content += `<div>${i}</div>`;
+    }
+    ele1.innerHTML = `<section class="nav__year-section">
+        <span>year</span>
+        <i class="fa fa-caret-left" aria-hidden="true"></i>
+        <span>2000+</span>
+        <i class="fa fa-caret-right" aria-hidden="true"></i>
+      </section>
+      <div class="nav__year-div">${content}</div>
+      <div class="nav__year-circle">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>`;
+    ele2.appendChild(ele1);
+  }
+  function gallery(ele1, ele2) {
+    ele1.innerHTML = `<div class="nav__gallery-div">
+        <section class="nav__gallery-section gallery-romance">
+          <div class="nav__img-div">
+            <figure class="nav__gallery-figure"></figure>
+            <figure class="nav__gallery-figure"></figure>
+            <figure class="nav__gallery-figure"></figure>
+          </div>
+          <div class="nav__gallery-line"></div>
+          <div class="nav__gallery-title">romances</div>
+        </section>
+        <section class="nav__gallery-section gallery-war">
+          <div class="nav__img-div">
+            <figure class="nav__gallery-figure"></figure>
+            <figure class="nav__gallery-figure"></figure>
+            <figure class="nav__gallery-figure"></figure>
+          </div>
+          <div class="nav__gallery-line"></div>
+          <div class="nav__gallery-title">wars</div>
+        </section>
+        <section class="nav__gallery-section gallery-fantasy">
+          <div class="nav__img-div">
+            <figure class="nav__gallery-figure"></figure>
+            <figure class="nav__gallery-figure"></figure>
+            <figure class="nav__gallery-figure"></figure>
+          </div>
+          <div class="nav__gallery-line"></div>
+          <div class="nav__gallery-title">fantasy</div>
+        </section>
+      </div>
+      <div class="nav__gallery-circle">
+          <div></div><div></div><div></div><div></div><div></div>
+      </div>
+      <a class="nav__gallery-a next-a"><span class="nav__gallery-span span-next"></span></a>
+      <a class="nav__gallery-a previous-a"><span class="nav__gallery-span span-previous"></span></a>`;
+    ele2.appendChild(ele1);
+  }
+  function news(ele1, ele2) {
+    let content = "";
+    for (let key in newsData) {
+      let tagContent = "";
+      for (let index in newsData[key].img) {
+        tagContent += `<div class="nav__news-Abox ${key}-news${+index + 1}">
+            <figure class="nav__news-figure" style="background-image: url('${
+              newsData[key].img[index]
+            }')"></figure>
+            <div class="nav__news-boxTitle">
+              <p class="nav__news-title">${newsData[key].title[index]}</p>
+              <div class="nav__news-line"></div>
+              <p class="nav__news-content">${newsData[key].content[
+                index
+              ].substring(0, 100) + "..."}</p>
+            </div>
+          </div>`;
+      }
+      content += `<section class="nav__news-section section-${key}">
+          <div class="nav__news-name">${key}</div>
+          <div class="nav__news-board newsboard-${key}">${tagContent}</div>
+          <div class="nav__news-arrow arrowDown-div">
+            <span class="nav__news-span arrowDown-span"></span>
+          </div>
+        </section>`;
+    }
+    ele1.innerHTML = `<div class="nav__news-child">${content}</div>
+      <div class="nav__news-circle">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>`;
+    ele2.appendChild(ele1);
+  };
+  function arrowLeft(ele1, ele2) {
+    ele1.innerHTML = `<span class="sideshow-primary__span span-arrow__left"></span>`;
+    ele2.appendChild(ele1);
+  };
+  function arrowRight(ele1, ele2) {
+    ele1.innerHTML = `<span class="sideshow-primary__span span-arrow__right"></span>`;
+    ele2.appendChild(ele1);
+  };
+  function ContentSideShow(ele1, ele2, index) {
+    let key = Object.keys(contentSideshow)[index];
+    ele1.innerHTML = `<h3 class="sideshow-primary__h3"><span>${contentSideshow[key].name}</span></h3>
+    <div class="sideshow-primary__line"></div>
+    <p class="sideshow-primary__p">${contentSideshow[key].content.substring(0, 130) + "..."}</p>`;
+    ele2.appendChild(ele1);
+  };
+  function containMark(ele1, ele2) {
+    let key = Object.keys(contentSideshow).length;
+    for(let i = 0; i < key; i++) {
+      if(i === 0)
+        ele1.innerHTML += `<div class="active"></div>`;
+      else
+        ele1.innerHTML += `<div></div>`;
+    }
+    ele2.appendChild(ele1);
+  };
+  function vendor(ele1, ele2) {
+    ele1.innerHTML = `<div class="panel-list__status">
+          <i class="fa fa-heart" aria-hidden="true"></i>
+          <span class="panel-list__ep">12/12</span>
+        </div>
+        <div class="panel-list__tag">
+          <i class="fa fa-tags" aria-hidden="true"></i>
+          <span class="panel-list__genre">action, adventure, mecha</span>
+        </div>
+      <div class="panel-list__play"></div>`;
+    ele2.appendChild(ele1);
+  }
+  return {
+    genre: genre,
+    release: release,
+    gallery: gallery,
+    news: news,
+    arrowLeft: arrowLeft,
+    arrowRight: arrowRight,
+    ContentSideShow: ContentSideShow,
+    containMark: containMark,
+    vendor: vendor
+  };
+})();
 
 let genre = new appendHTML(
   "div",
   ".nav__div.nav__genre",
-  "content",
-  "nav__genre--slider"
+  "nav__genre--slider",
+  ApplyContent.genre
 );
-
-genre.modifier = `<div class="div-genre">action</div>
-    <div class="div-line"></div>
-    <div class="div-genre">adventure</div>
-    <div class="div-line"></div>
-    <div class="div-genre">tragedy</div>
-    <div class="div-line"></div>
-    <div class="div-genre">history</div>
-    <div class="div-line"></div>
-    <div class="div-genre">horror</div>
-    <div class="div-line"></div>
-    <div class="div-genre">mystery</div>
-    <div class="div-line"></div>
-    <div class="div-genre">spsychology</div>
-    <div class="div-line"></div>
-    <div class="div-genre">cartoon</div>
-    <div class="div-circle">
-    <div></div><div></div><div></div><div></div><div></div>
-    </div>`;
-
 genre.apply();
-
-appendHTML.prototype.event = function(callback1, callback2, eleTarget) {
-  callback1(this._tagHTML, eleTarget);
-  callback2(this._tagHTML, eleTarget);
+appendHTML.prototype.event = function(callback1, callback2, eleDisplay, eleTarget) {
+  callback1(eleDisplay, eleTarget);
+  callback2(eleDisplay, eleTarget);
 };
 
 const eventCallback = (function() {
   function click(ele, eleTarget) {
+    let eleDisplay = document.querySelector(ele);
     let domTarget = document.querySelector(eleTarget);
-    domTarget.addEventListener("click",()=>{
-      if (ele.style.display == "block") {
-        ele.style.display = "none";
-        ele.style.opacity = 0;
+    domTarget.addEventListener("click", () => {
+      if (eleDisplay.style.visibility == "visible") {
+        eleDisplay.style.visibility = "hidden";
+        eleDisplay.style.opacity = 0;
         domTarget.classList.remove("active");
       } else {
-        ele.style.display = "block";
-        ele.style.opacity = 1;
+        eleDisplay.style.visibility = "visible";
+        eleDisplay.style.opacity = 1;
         domTarget.classList.add("active");
       }
     });
   }
-  function windowClick(ele, eleTarget){
+  function windowClick(ele, eleTarget) {
+    let eleDisplay = document.querySelector(ele);
     let domTarget = document.querySelector(eleTarget);
-    window.addEventListener("click", (e)=>{
-      if(!ele.contains(e.target) && !domTarget.contains(e.target)){
-        ele.style.display = "none";
-        ele.style.opacity = 0;
+    window.addEventListener("click", e => {
+      if (!eleDisplay.contains(e.target) && !domTarget.contains(e.target)) {
+        eleDisplay.style.visibility = "hidden";
+        eleDisplay.style.opacity = 0;
         domTarget.classList.remove("active");
       }
     });
   }
+  function clicksideShow(btn1Target, btn2Target) {
+    let btn1 = document.querySelector(btn1Target);
+    let btn2 = document.querySelector(btn2Target);
+    let eleTarget1 = document.querySelectorAll(arguments[2])
+    let eleTarget2 = document.querySelectorAll(arguments[3]);
+    let count = 0;
+    btn1.addEventListener("click", ()=>{
+      eleTarget1[count].classList.add("middle");
+      count++;
+      count >= eleTarget1.length? count = 0: "";
+      eleTarget1.forEach((particle,index)=>{
+        index === count? particle.classList.add("active"): particle.classList.remove("active");
+      });
+      eleTarget1[count].addEventListener("transitionend", function observer(){
+        eleTarget1[count - 1 < 0? eleTarget1.length - 1: count - 1].classList.remove("middle");
+        eleTarget1[count].removeEventListener("transitionend", observer);
+      });
+      eleTarget2.forEach((particle,index)=>{
+        index === count? particle.classList.add("active"): particle.classList.remove("active");
+      });
+    });
+    btn2.addEventListener("click", ()=>{
+      eleTarget1[count].classList.add("middle");
+      count--;
+      count < 0 ? count = eleTarget1.length - 1: "";
+      eleTarget1.forEach((particle,index)=>{
+        index === count? particle.classList.add("active"): particle.classList.remove("active");
+      });
+      eleTarget1[count].addEventListener("transitionend", function observer(){
+        eleTarget1[count + 1 > eleTarget1.length - 1? 0 : count + 1].classList.remove("middle");
+        eleTarget1[count].removeEventListener("transitionend", observer);
+      });
+      eleTarget2.forEach((particle,index)=>{
+        index === count? particle.classList.add("active"): particle.classList.remove("active");
+      });
+    });
+  }
   return {
     click: click,
-    clickOut: windowClick
+    clickOut: windowClick,
+    clicksideShow: clicksideShow
   };
 })();
 
-genre.event(eventCallback.click, eventCallback.clickOut, ".nav__div.nav__genre");
+genre.event(
+  eventCallback.click,
+  eventCallback.clickOut,
+  ".nav__genre--slider",
+  ".nav__div.nav__genre"
+);
 
-let release = new appendHTML("div", "nav.nav", "content", "nav__release_js");
-release.modifier = function(){
-  let content = "";
-  for(let i = 2000; i <= 2020; i++) {
-    content += `<div>${i}</div>`;
-  }
-  return `<section class="nav__year-section">
-      <span>year</span>
-      <i class="fa fa-caret-left" aria-hidden="true"></i>
-      <span>2000+</span>
-      <i class="fa fa-caret-right" aria-hidden="true"></i>
-    </section>
-    <div class="nav__year-div">${content}</div>
-    <div class="nav__year-circle">
-      <div></div><div></div><div></div><div></div><div></div>
-    </div>`
-}();
-
+let release = new appendHTML(
+  "div",
+  "nav.nav",
+  "nav__release_js",
+  ApplyContent.release
+);
 release.apply();
-release.event(eventCallback.click, eventCallback.clickOut, ".nav__div.nav__release");
+release.event(
+  eventCallback.click,
+  eventCallback.clickOut,
+  ".nav__release_js",
+  ".nav__div.nav__release"
+);
 
-let gallery = new appendHTML("div", "nav.nav", "content", "nav__setGallery");
-gallery.modifier = `<div class="nav__gallery-div">
-    <section class="nav__gallery-section gallery-romance">
-      <div class="nav__img-div">
-        <figure class="nav__gallery-figure"></figure>
-        <figure class="nav__gallery-figure"></figure>
-        <figure class="nav__gallery-figure"></figure>
-      </div>
-      <div class="nav__gallery-line"></div>
-      <div class="nav__gallery-title">romances</div>
-    </section>
-    <section class="nav__gallery-section gallery-war">
-      <div class="nav__img-div">
-        <figure class="nav__gallery-figure"></figure>
-        <figure class="nav__gallery-figure"></figure>
-        <figure class="nav__gallery-figure"></figure>
-      </div>
-      <div class="nav__gallery-line"></div>
-      <div class="nav__gallery-title">wars</div>
-    </section>
-    <section class="nav__gallery-section gallery-fantasy">
-      <div class="nav__img-div">
-        <figure class="nav__gallery-figure"></figure>
-        <figure class="nav__gallery-figure"></figure>
-        <figure class="nav__gallery-figure"></figure>
-      </div>
-      <div class="nav__gallery-line"></div>
-      <div class="nav__gallery-title">fantasy</div>
-    </section>
-  </div>
-  <div class="nav__gallery-circle">
-      <div></div><div></div><div></div><div></div><div></div>
-  </div>
-  <div class="nav__gallery-button">
-    <button class="nav__gallery-button1">
-    <i class="fa fa-chevron-left" aria-hidden="true"></i>
-    </button>
-    <button class="nav__gallery-button2">
-    <i class="fa fa-chevron-right" aria-hidden="true"></i>
-    </button>
-  </div>`
-
+let gallery = new appendHTML(
+  "div",
+  "nav.nav",
+  "nav__setGallery",
+  ApplyContent.gallery
+);
 gallery.apply();
-gallery.event(eventCallback.click, eventCallback.clickOut, ".nav__div.nav__gallery");
+gallery.event(
+  eventCallback.click,
+  eventCallback.clickOut,
+  ".nav__setGallery",
+  ".nav__div.nav__gallery"
+);
+
+let news = new appendHTML(
+  "div",
+  "nav.nav",
+  "nav__news-primary",
+  ApplyContent.news
+);
+news.apply();
+
+const moveStyle = (function() {
+  function translate(eleTarget, btn1) {
+    let domTarget = document.querySelectorAll(eleTarget);
+    let btn1Target = document.querySelector(btn1);
+    let css = getComputedStyle(domTarget[0]);
+    let widthMove = domTarget[0].clientHeight + parseInt(css.marginTop) * 2;
+    let count = 0;
+    btn1Target.addEventListener("click", () => {
+      count--;
+      count < 0 ? (count = 2) : "";
+      domTarget[count].style.transform = `translateY(-${widthMove *
+        (count + 1)}px)`;
+      domTarget[count + 1 > 2 ? 0 : count + 1].style.zIndex = 3;
+      domTarget.forEach((particle, index) => {
+        index == (count + 1 > 2 ? 0 : count + 1)
+          ? ""
+          : (particle.style.zIndex = 0);
+        let transStyle = particle.style.transform
+          ? parseInt(particle.style.transform.split("(")[1])
+          : 0;
+        particle.style.transform = `translateY(${transStyle + widthMove}px)`;
+      });
+    });
+  }
+  function translate2(nameEle, btn) {
+    let eleTarget = document.querySelectorAll(nameEle);
+    let btnTarget = document.querySelector(btn);
+    let css = getComputedStyle(eleTarget[0]);
+    let transMove = eleTarget[0].offsetHeight + parseInt(css.marginTop) * 2;
+    console.log(transMove);
+    eleTarget.forEach((particle, index) => {
+      particle.style.zIndex = 2 - index;
+      if (index !== 0)
+        particle.style.transform = `translateY(-${(index - 1) * transMove}px)`;
+    });
+    let count = 0;
+    btnTarget.addEventListener("click", () => {
+      let transStyle = eleTarget[count].style.transform
+        ? parseInt(eleTarget[count].style.transform.split("(")[1])
+        : 0;
+      eleTarget[count].style.transform = `translateY(${transStyle +
+        transMove}px)`;
+      eleTarget[
+        count + 1 > 2 ? 0 : count + 1
+      ].style.transform = `translateY(${parseInt(
+        eleTarget[count + 1 > 2 ? 0 : count + 1].style.transform.split("(")[1]
+      ) - transMove}px)`;
+      count++;
+      count > 2 ? (count = 0) : "";
+      eleTarget[count].addEventListener("transitionend", function myevent(e) {
+        e.currentTarget.style.zIndex = 2;
+        eleTarget.forEach((particle, index) => {
+          if (index !== count) {
+            particle.style.zIndex = 0;
+          }
+        });
+        e.currentTarget.removeEventListener("transitionend", myevent);
+      });
+    });
+  }
+  return { translate: translate, translate2: translate2 };
+})();
+
+news.event(eventCallback.click, eventCallback.clickOut, ".nav__news-primary", ".nav__div.nav__news");
+moveStyle.translate2(
+  ".section-anime .nav__news-Abox",
+  ".section-anime .arrowDown-div"
+);
+moveStyle.translate2(
+  ".section-movie .nav__news-Abox",
+  ".section-movie .arrowDown-div"
+);
+moveStyle.translate2(
+  ".section-Tvseries .nav__news-Abox",
+  ".section-Tvseries .arrowDown-div"
+);
+
+let sideshowArrowLeft = new appendHTML(
+  "div",
+  ".sideshow-primary",
+  "sideshow-primary__arrow div-arrow__left",
+  ApplyContent.arrowLeft
+);
+sideshowArrowLeft.apply();
+let sideshowArrowRight = new appendHTML(
+  "div",
+  ".sideshow-primary",
+  "sideshow-primary__arrow div-arrow__right",
+  ApplyContent.arrowRight
+);
+sideshowArrowRight.apply();
+
+let ContentSideShow = new appendHTML(
+  "div",
+  ".sideshow-primary__div",
+  "sideshow-primary__content",
+  ApplyContent.ContentSideShow
+);
+ContentSideShow.apply();
+
+let containMark = new appendHTML ("div", ".sideshow-primary", "sideshow-primary__mark",ApplyContent.containMark);
+containMark.apply()
+
+eventCallback.clicksideShow(".sideshow-primary__arrow.div-arrow__left", ".sideshow-primary__arrow.div-arrow__right", ".sideshow-primary__div", ".sideshow-primary__mark > div");
+
+let hoverPlay = new appendHTML("div", ".sideshow-popular__figure, .sideshow-favourite__figure, .sideshow-latest__figure, .sideshow-editorChoice__figure, .sideshow-filmSoon__figure", "panel-list__vendor", ApplyContent.vendor);
+hoverPlay.apply();
---
+++ project_anime/JS/data.js
@@ -0,0 +1,61 @@
+const DataWeb = [
+    {
+        anime: {
+            img: ["IMAGE/news/princess-principal-movie.jpg", "IMAGE/news/Fruits-Basket.jpg", "IMAGE/news/IDOLiSH7.jpg"],
+            title: ["princess principal new movie", "Fruits Basket season 2", "IDOLiSH7 season2"],
+            content: ["Inspired by Takaya Natsuki’s manga, the series follows high school girl Honda Tohru, who finds herself moving in with classmate Sohma Yuki after her mother dies - only to discover that he is the victim of a curse in which 12 members of his family are possessed by spirits of the Chinese Zodiac.", "The first installment of Princess Principal: Crown Handler, a series of theatrical movie sequels to the Princess Principal TV anime, has released a trailer featuring its opening theme plus plot details ahead of its Apr. 10 release!", "Season 2 of popular anime IDOLiSH7 is set to air from Apr. 5, with its first broadcast to include two episodes! A commercial narrated by young idol Nanase Riku has been released, previewing the boys' new journey."]
+        },
+        movie: {
+            img: ["IMAGE/news/onward.jpg", "IMAGE/news/peter-rabbit.jpg", "IMAGE/news/artemis-fowl-1576601149.jpg"],
+            title: ["Onward", "Peter Rabbit 2: The Runaway", "Artemis Fowl"],
+            content: ["The first Disney movie of the year comes from Pixar, and follows a pair of elf brothers who venture into the world to see if they can still find some magic in it", "The tales of the mischievous rabbit in the blue coat returns for more Beatrix Potter-inspired fun, with James Corden providing the voice of the scheming bunny again", "Based on the beloved book series by Eoin Colfer, the movie follows the adventures of a kid who's also a criminal mastermind. This time, he's off to find — and rob — a secret, hidden group of fairies."]
+        },
+        Tvseries: {
+            img: ["IMAGE/news/Ugly-Delicious-Season-2.png", "IMAGE/news/The-Protector-Season-3-Coming-to-Netflix-March-2020-696x392.jpg", "IMAGE/news/castlevania-season-3-poster-netflix.jpg"],
+            title: ["Ugly Delicious Season 2", "The Protector Season 3", "castlevania season 3"],
+            content: ["This time around there are some changes, as lead host and creator David Chang becomes a father and the first episode documents that entire journey. While there is the usual foodie deliciousness to follow in the second series, it is interspersed with David’s personal life more so than the first season.", "Season 2 of Netflix’s first original Turkish series, The Protector arrived in April, Friday 26. Fans of the show, including myself, cannot wait for the third installment. All right episodes came out on Friday, and people binged the TV show like mad", "Netflix has officially renewed its Castlevania animated series for a third season. Season 3 will be the show’s longest yet, with 10 episodes of vampire-killing action."]
+            }
+    },
+    {
+        film1: {
+            name: "Boku dake ga Inai Machi",
+            content: `When tragedy is about to strike, Satoru Fujinuma finds himself sent back several minutes before the accident occurs. The detached, 29-year-old manga artist has taken advantage of this powerful yet mysterious phenomenon, which he calls "Revival," to save many lives.`
+        },
+        film2: {
+            name: "Darker than Black: Kuro no Keiyakusha",
+            content: `It has been 10 years since Heaven's Gate appeared in South America and Hell's Gate appeared in Japan, veiling the once familiar night sky with an oppressive skyscape. Their purposes unknown, these Gates are spaces in which the very laws of physics are ignored. With the appearance of the Gates emerged Contractors, who, in exchange for their humanity, are granted supernatural abilities.`
+        },
+        film3: {
+            name: "Durarara!!",
+            content: `In Tokyo's downtown district of Ikebukuro, amidst many strange rumors and warnings of anonymous gangs and dangerous occupants, one urban legend stands out above the rest—the existence of a headless "Black Rider" who is said to be seen driving a jet-black motorcycle through the city streets.`
+        },
+        film4: {
+            name: "Boku no Hero Academia",
+            content: `The appearance of "quirks," newly discovered super powers, has been steadily increasing over the years, with 80 percent of humanity possessing various abilities from manipulation of elements to shapeshifting. This leaves the remainder of the world completely powerless, and Izuku Midoriya is one such individual.`
+        },
+        film5: {
+            name: "Psycho-Pass",
+            content: `Justice, and the enforcement of it, has changed. In the 22nd century, Japan enforces the Sibyl System, an objective means of determining the threat level of each citizen by examining their mental state for signs of criminal intent, known as their Psycho-Pass. Inspectors uphold the law by subjugating, often with lethal force, anyone harboring the slightest ill-will; alongside them are Enforcers, jaded Inspectors that have become latent criminals, granted relative freedom in exchange for carrying out the Inspectors' dirty work.`
+        }
+    },
+    {
+        popular: {
+            film1: {
+                name: "Code Geass: Hangyaku no Lelouch",
+                genre: ["Action", "Military", "Sci-Fi", "Super Power", "Drama", "Mecha", "School"],
+                release: "Oct 6, 2006",
+                Studios: "Sunrise",
+                country: "japan",
+                Type: "Tv series",
+                Duration: "24 minute",
+                Episodes: 25,
+                content: `In the year 2010, the Holy Empire of Britannia is establishing itself as a dominant military nation, starting with the conquest of Japan. Renamed to Area 11 after its swift defeat, Japan has seen significant resistance against these tyrants in an attempt to regain independence. Lelouch Lamperouge, a Britannian student, unfortunately finds himself caught in a crossfire between the Britannian and the Area 11 rebel armed forces. He is able to escape, however, thanks to the timely appearance of a mysterious girl named C.C., who bestows upon him Geass, the "Power of Kings." Realizing the vast potential of his newfound "power of absolute obedience," Lelouch embarks upon a perilous journey as the masked vigilante known as Zero, leading a merciless onslaught against Britannia in order to get revenge once and for all.`
+            },
+            film2: {
+                name: "dasfd"
+            }
+        }
+    }
+];
+
+[newsData, contentSideshow] = DataWeb;
