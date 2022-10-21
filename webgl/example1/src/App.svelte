215177ce6368e436d304457e385e9903baad35db
lanhsotret <58644190+lanhsotret@users.noreply.github.com>
2022-10-21 12:17:14 +0700

    add

---
+++ webgl/example1/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import {InitApp} from "./lib/canvas";
+  import { onMount } from 'svelte';
+
+  //Chunmomo
+  
+  onMount(async () => {
+		let canvas = new InitApp({radius: 200});
+    canvas.create().then(console.log);
+    // canvas.draw();
+    // let test = new Grid(100);
+    // test.draw()
+	});
+</script>
+
+
+<div id="pixi"></div>
+<div id="test">
+  <canvas></canvas>
+</div>
+
+<style lang="css">
+  #pixi {
+    width: 100vw;
+    height: 100vh;
+  }
+  #test {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
